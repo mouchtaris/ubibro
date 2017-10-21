b02aa80078fe58a6ad06f2b3a017f2823c8c99e8
Nikos Mouchtaris <nikos.mouchtaris@redtulipsystems.com>
2017-10-21 13:58:32 +0000

    Remove debugging flags from compilation

--- project/ScalacOptions.scala
+++ project/ScalacOptions.scala
@@ -16,8 +16,8 @@ object ScalacOptions {
     "-unchecked",                        // Enable additional warnings where generated code depends on assumptions.
     "-Xcheckinit",                       // Wrap field accessors to throw an exception on uninitialized access.
 //    "-Xfatal-warnings",                  // Fail the compilation if there are any warnings.
-    "-Xprint:typer",
-    "-Xlog-implicits",
+//    "-Xprint:typer",
+//    "-Xlog-implicits",
     "-Xfuture",                          // Turn on future language features.
     "-Xlint:adapted-args",               // Warn if an argument list is modified to match the receiver.
     "-Xlint:by-name-right-associative",  // By-name parameter of right associative operator.
