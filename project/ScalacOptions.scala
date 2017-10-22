22dfa250ab1437636a3359aebd10bc4aab1f0c25
Nikos Mouchtaris <nikos.mouchtaris@redtulipsystems.com>
2017-10-22 15:02:10 +0000

    Debugging scalac options

--- project/ScalacOptions.scala
+++ project/ScalacOptions.scala
@@ -17,7 +17,7 @@ object ScalacOptions {
     "-Xcheckinit",                       // Wrap field accessors to throw an exception on uninitialized access.
 //    "-Xfatal-warnings",                  // Fail the compilation if there are any warnings.
 //    "-Xprint:typer",
-//    "-Xlog-implicits",
+    "-Xlog-implicits",
     "-Xfuture",                          // Turn on future language features.
     "-Xlint:adapted-args",               // Warn if an argument list is modified to match the receiver.
     "-Xlint:by-name-right-associative",  // By-name parameter of right associative operator.
