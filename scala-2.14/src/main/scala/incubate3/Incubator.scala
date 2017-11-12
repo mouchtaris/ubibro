bea0030d3ccd8cec9cbc144de85408904debfb62
Nikos Mouchtaris <nikos.mouchtaris@redtulipsystems.com>
2017-11-12 19:44:38 +0000

    wip

--- scala-2.14/src/main/scala/incubate3/Incubator.scala
+++ scala-2.14/src/main/scala/incubate3/Incubator.scala
@@ -23,24 +23,7 @@ object hell0 {
 
   }
 
-  object ghosts {
-
-    class Getter[ev[_ <: U], U](val self: Unit) extends AnyVal {
-      def apply[t <: U: ev](): ev[t] = implicitly
-    }
-
-    trait ResultOf2Ext[U] extends Any {
-      type on[a, b] = {
-        type t[_ <: U]
-      }
-    }
-
-    trait Getter2[U] {
-      val resultOf: ResultOf2Ext[U]
-      def apply[a, b] = new Getter[resultOf.on[a, b]#t, U](())
-    }
-
-  }
+  object ghosts
 
   object evidence {
 
@@ -61,15 +44,19 @@ object hell0 {
     final type ||[a, b] = Or[a, b, _]
     sealed trait OrResultOf[a, b]
       extends Any
-      with ghosts.ResultOfExt[Any]
       { final type t[out] = Or[a, b, out] }
     object Or
       extends AnyRef
       with OrDeductionA
-      with ghosts.Getter2[OrResultOf, Any]
     {
       final type resultOf[a, b] = OrResultOf[a, b]
       implicit def fromUnit[a, b, ev](u: Unit): Or[a, b, ev] = new Or(u)
+
+      final class Getter[a, b](val self: Unit) extends AnyVal {
+        def apply[out: resultOf[a, b]#t](): Or[a, b, out] = ()
+      }
+
+      def apply[a, b] = new Getter[a, b](())
     }
 
     final implicit class &&[a, b](val self: Unit) extends AnyVal
@@ -84,7 +71,6 @@ object hell0 {
     final class Map[f[_], list <: List, out <: List](val self: Unit) extends AnyVal
     sealed trait MapResultOf[f[_], list <: List]
       extends Any
-      with ghosts.ResultOfExt[List]
       { type t[out <: List] = Map[f, list, list] }
 
   }
